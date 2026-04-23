e54cff0c43d0fa6663955ecafa89195e820a9993
Rogerio <rogerioborgesassuncao@gmail.com>
2026-04-23 13:23:03 -0300

    fix: atualiza SVG do instalar app

--- components/pwa-register.tsx
+++ components/pwa-register.tsx
@@ -106,13 +106,21 @@ export function PWARegister() {
                 display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'center',
-                fontWeight: 800,
-                fontSize: 14,
-                color: '#0a0a0a',
                 flexShrink: 0,
+                overflow: 'hidden',
               }}
             >
-              CA
+              <svg width="36" height="36" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg" style={{display:'block'}}>
+                <defs>
+                  <style>{`@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@900&display=swap');`}</style>
+                </defs>
+                <g transform="translate(33, 0)">
+                  <text x="0" y="346" fontFamily="Raleway, Arial Black, sans-serif" fontWeight="900" fontSize="248" fill="#000000">C</text>
+                  <path d="M 182,346 L 214,346 L 254,167 L 222,167 Z" fill="#FFFFFF"/>
+                  <path d="M 230,346 L 262,346 L 302,167 L 270,167 Z" fill="#FFFFFF"/>
+                  <text x="277" y="346" fontFamily="Raleway, Arial Black, sans-serif" fontWeight="900" fontSize="248" fill="#000000">A</text>
+                </g>
+              </svg>
             </div>
             <div>
               <p style={{ fontSize: 13, fontWeight: 600, color: 'var(--txt)' }}>
